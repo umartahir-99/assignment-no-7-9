5bf142110bbfbd986f24911f91e32286b0c18a0f
umartahir-99 <tahirumar835@gmail.com>
2024-12-21 20:28:31 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+
+// Give outfit suggestions based on weather conditions.
+// Task:
+// Take weather input (Sunny, Rainy, Cold) from the user.
+// Print:
+// "Wear sunglasses" for Sunny
+// "Take an umbrella" for Rainy
+// "Wear a jacket" for Cold
+
+
+var wheather =  prompt("Enter wheather (Sunny, Rainy, Cold):");
+
+if(wheather == "Sunny"){
+  console.log("Wear sunglasses")
+
+}
+else if(wheather == "Rainy"){
+   console.log("Take an Umbrella")
+}
+else if(wheather == "Cold"){
+  console.log("Wear a jacket")
+
+}
+
+
+
